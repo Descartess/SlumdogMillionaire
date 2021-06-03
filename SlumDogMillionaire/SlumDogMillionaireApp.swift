345b93aadd57ad980e46699fd2c9dd8a892f509b
Paul  Nyondo <nyondopaul23@gmail.com>
2021-06-03 10:38:31 +0300

    Initial Commit

---
+++ SlumDogMillionaire/SlumDogMillionaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlumDogMillionaireApp.swift
+//  SlumDogMillionaire
+//
+//  Created by Paul Nyondo on 03/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SlumDogMillionaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
