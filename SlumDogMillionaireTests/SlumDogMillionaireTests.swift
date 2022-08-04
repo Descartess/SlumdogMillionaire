50491e1fdb47233c466e0aa684e665799340a44d
Paul Nyondo <nyondopaul23@gmail.com>
2022-08-04 11:21:54 +0300

    Update tests

--- SlumDogMillionaireTests/SlumDogMillionaireTests.swift
+++ SlumDogMillionaireTests/SlumDogMillionaireTests.swift
@@ -17,68 +17,71 @@ class SlumDogMillionaireTests: XCTestCase {
         let scheduler = DispatchQueue.test
         let store = TestStore(initialState: AppState(),
                               reducer: appReducer,
-                              environment: AppEnvironment(loadQuestions: { _ in Effect(value: testQuestionBank)},
+                              environment: AppEnvironment(loadQuestions: {  Effect(value: testQuestionBank)},
                                                           bundle: Bundle(for: type(of: self)),
                                                           mainQueue: scheduler.eraseToAnyScheduler()))
 
-        store.send(.loadGame)
+        store.send(.loadGame) {
+            $0.isLoading = true
+        }
 
         scheduler.advance(by: 1)
 
         store.receive(.startGame(.success(testQuestionBank))) {
             $0.levelState = testInitialLevelState
+            $0.isLoading = false
         }
 
         store.receive(.level(action: .timerTicked)) {
             $0.levelState?.answerPeriod = 59
         }
 
-        store.assert( .send(.endGame) {
+        store.send(.endGame) {
             $0.levelState = nil
-        })
+        }
     }
 
     func testEndingGame() {
         let store = TestStore(initialState: AppState(levelState: testInitialLevelState),
                               reducer: appReducer,
-                              environment: AppEnvironment(loadQuestions: { _ in Effect(value: testQuestionBank)},
+                              environment: AppEnvironment(loadQuestions: {  Effect(value: testQuestionBank)},
                                                           bundle: Bundle(for: type(of: self)),
                                                           mainQueue: self.scheduler.eraseToAnyScheduler()))
 
-        store.assert( .send(.endGame) {
+        store.send(.endGame) {
             $0.levelState = nil
-        })
+        }
     }
 
     func testAttemptCorrectAnswerToLevel() {
         let store = TestStore(initialState: AppState(levelState: testInitialLevelState),
                               reducer: appReducer,
-                              environment: AppEnvironment(loadQuestions: { _ in Effect(value: testQuestionBank)},
+                              environment: AppEnvironment(loadQuestions: {  Effect(value: testQuestionBank)},
                                                           bundle: Bundle(for: type(of: self)),
                                                           mainQueue: self.scheduler.eraseToAnyScheduler()))
 
-        store.assert( .send(.level(action: .attempt(solution: 1))) {
+        store.send(.level(action: .attempt(solution: 1))) {
             $0.levelState?.levelPassed = true
-        })
+        }
     }
 
     func testAttemptWrongAnswerToLevel() {
         let store = TestStore(initialState: AppState(levelState: testInitialLevelState),
                               reducer: appReducer,
-                              environment: AppEnvironment(loadQuestions: { _ in Effect(value: testQuestionBank)},
+                              environment: AppEnvironment(loadQuestions: {  Effect(value: testQuestionBank)},
                                                           bundle: Bundle(for: type(of: self)),
                                                           mainQueue: self.scheduler.eraseToAnyScheduler()))
 
-        store.assert( .send(.level(action: .attempt(solution: 2))) {
+        store.send(.level(action: .attempt(solution: 2))) {
             $0.levelState?.levelPassed = false
-        })
+        }
     }
 
     func testAdvanceToNextLevel() {
         let scheduler = DispatchQueue.test
         let store = TestStore(initialState: AppState(levelState: testInitialLevelState),
                               reducer: appReducer,
-                              environment: AppEnvironment(loadQuestions: { _ in Effect(value: testQuestionBank)},
+                              environment: AppEnvironment(loadQuestions: {  Effect(value: testQuestionBank)},
                                                           bundle: Bundle(for: type(of: self)),
                                                           mainQueue: scheduler.eraseToAnyScheduler()))
         store.send(.level(action: .next)) {
@@ -89,12 +92,12 @@ class SlumDogMillionaireTests: XCTestCase {
 
         scheduler.advance(by: 2)
 
-        store.receive(.level(action: .timerTicked)) {
-            $0.levelState?.answerPeriod = 59
-        }
+//        store.receive(.level(action: .timerTicked)) {
+//            $0.levelState?.answerPeriod = 59
+//        }
 
-        store.assert( .send(.endGame) {
+        store.send(.endGame) {
             $0.levelState = nil
-        })
+        }
     }
 }
